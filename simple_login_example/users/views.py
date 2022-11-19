085573376714931f49d6cc2ddcd35cc9e24834b7
simjeehoon <simjeehoon@naver.com>
2022-11-19 11:27:09 +0900

    7주차 과제

--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 import json
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 def login(request: HttpRequest):
@@ -11,16 +12,26 @@ def login(request: HttpRequest):
     }
 
     if request.method == 'GET':
-        username = request.GET['username']
-        password = request.GET['password']
+        return render(request, 'users/login.html')
+
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # 유저가 아이디나 비밀번호를 입력하는 상황에서 실수롤 정보를 다 기입하지 않은 상황
+        if username == '':
+            return HttpResponse('유저 아이디를 입력해주세요')
+        if password == '':
+            return HttpResponse('유저 비밀번호를 입력해주세요')
 
         if username != user_data['username']:
             return HttpResponse('유저 아이디가 올바르지 않습니다.')
-        
         if password != user_data['password']:
             return HttpResponse('유저 비밀번호가 올바르지 않습니다.')
-
+        
         return HttpResponse('로그인 성공!')
+
+    
 
 def login_detail(request: HttpRequest, id):
     return HttpResponse(f"user id는 {id}입니다.")
