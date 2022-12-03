d823f01a473a651c14157d5486509f402e433972
simjeehoon <simjeehoon@naver.com>
2022-12-03 12:40:59 +0900

    week9

--- simple_login_example/users/urls.py
+++ simple_login_example/users/urls.py
@@ -6,5 +6,7 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('login/<int:id>/', views.login_detail, name='login-detail'),
-    
+    path('login/index', views.logout, name='index'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup')
 ]
--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 
+from users.models import User
 import json
 from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 def login(request: HttpRequest):
-    user_data = {
-        'username': 'python',
-        'password': 'django'
-    }
-
     context = {
         'method': request.method,
         'is_valid': True
@@ -23,28 +19,66 @@ def login(request: HttpRequest):
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        # 유저가 아이디나 비밀번호를 입력하는 상황에서 실수롤 정보를 다 기입하지 않은 상황
-        if username == '':
-            context['is_valid'] = False
-        if password == '':
-            context['is_valid'] = False
-
-        if username != user_data['username']:
-            context['is_valid'] = False
-        if password != user_data['password']:
-            context['is_valid'] = False
-        
-        if context['is_valid']:
+        try:
+            user = User.objects.get(username=username, password=password)
             responce = redirect('pages:index')
             
-            responce.set_cookie('username', user_data['username'])
-            responce.set_cookie('password', user_data['password'])
+            responce.set_cookie('username', user.username)
+            responce.set_cookie('password', user.password)
             responce.set_cookie('is_login', True)
-            
-            return responce
 
-        return render(request, 'users/login.html', context)
+            return responce
+        except User.DoesNotExist:  # 회원 정보가 없는 경우
+            context['is_valid'] = False
+            return render(request, 'users/login.html', context)
+        
+        
+
+        
 
 
 def login_detail(request: HttpRequest, id):
     return HttpResponse(f"user id는 {id}입니다.")
+
+def index(request):
+    return render(request, 'index.html')
+
+def logout(request):
+    response = redirect('pages:index')
+    response.delete_cookie('is_login')
+    response.delete_cookie('username')
+    response.delete_cookie('password')
+
+    return response
+
+def signup(request):
+    context = {
+        'blank': False,
+        'exist': False
+    }
+
+    template_name = 'users/signup.html'
+
+    if request.method == "GET":
+        return render(request, template_name, context)
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # 빈칸 여부 확인
+        if username == '' or password == '':
+            context['blank'] = True
+            return render(request, template_name, context)
+        
+        
+        exist = User.objects.filter(username=username).exists()
+        if exist: # 아이디가 이미 존재할때
+            context['exist'] = exist
+            return render(request, template_name, context)
+
+
+        User.objects.create(username=username, password=password)
+        return redirect('users:login')
+
+    
